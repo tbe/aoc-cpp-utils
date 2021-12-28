e0d883ce3e935944a2851be1037bf2cfd85e2b27
Thomas Berger <loki@lokis-chaos.de>
2021-12-28 22:37:34 +0100

    Fixed another copy-paste error in the AoC CMake function.

--- cmake/AoC-Helpers.cmake
+++ cmake/AoC-Helpers.cmake
@@ -1,8 +1,8 @@
 function(add_aoc_executable HEADER CLASS DAY YEAR)
-    file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/puzzle-${year}-${day}.cpp             "#include <puzzle.h>
+    file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/puzzle-${YEAR}-${DAY}.cpp             "#include <puzzle.h>
 #include \"${CMAKE_CURRENT_SOURCE_DIR}/${HEADER}\"
 
  PUZZLE_MAIN(${YEAR}, ${DAY}, ${CLASS})")
-    add_executable(aoc-${year}-${day} ${CMAKE_CURRENT_BINARY_DIR}/puzzle.cpp)
-    target_link_libraries(aoc-${year}-${day} aoc_puzzle)
+    add_executable(aoc-${YEAR}-${DAY} ${CMAKE_CURRENT_BINARY_DIR}/puzzle.cpp)
+    target_link_libraries(aoc-${YEAR}-${DAY} aoc_puzzle)
 endfunction()
