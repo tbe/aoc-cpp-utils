e81c8734e3d9be2ccb37cdcfb8a400c3ed0d7ad6
Thomas Berger <loki@lokis-chaos.de>
2021-12-28 22:39:24 +0100

    Anothoer AoC CMake fix ...

--- cmake/AoC-Helpers.cmake
+++ cmake/AoC-Helpers.cmake
@@ -2,7 +2,7 @@ function(add_aoc_executable HEADER CLASS DAY YEAR)
     file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/puzzle-${YEAR}-${DAY}.cpp             "#include <puzzle.h>
 #include \"${CMAKE_CURRENT_SOURCE_DIR}/${HEADER}\"
 
- PUZZLE_MAIN(${YEAR}, ${DAY}, ${CLASS})")
-    add_executable(aoc-${YEAR}-${DAY} ${CMAKE_CURRENT_BINARY_DIR}/puzzle.cpp)
+PUZZLE_MAIN(${YEAR}, ${DAY}, ${CLASS})")
+    add_executable(aoc-${YEAR}-${DAY} ${CMAKE_CURRENT_BINARY_DIR}/puzzle-${YEAR}-${DAY}.cpp)
     target_link_libraries(aoc-${YEAR}-${DAY} aoc_puzzle)
 endfunction()
